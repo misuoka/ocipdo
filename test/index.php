5cd531428ad983ae1b7a38ccd22e869bf0925317
misuoka <2214787353@qq.com>
2019-04-24 21:00:23 +0800

    modify

--- test/index.php
+++ test/index.php
@@ -4,6 +4,6 @@ require_once './../vendor/autoload.php'; // 加载自动加载文件
 
 use ocipdo\PDO as OCIPDO;
 
-$pdo = new OCIPDO('oci:dbname=(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.207.33.86)(PORT = 1521)))(CONNECT_DATA=(SID=orcl)))', 'pan', 'panpan');
+$pdo = new OCIPDO('oci:dbname=//', 'pan', 'panpan');
 
 var_dump($pdo);
